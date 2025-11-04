d713bd9f6d9c7d1b4cb3c0f59faf5b8776b4014d
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-11-04 14:47:48 +0530

    Create Program14_4.c
    This program counts how many times the digit `4` appears in a given integer. It first handles negative numbers by converting them to positive. Then, using a `while` loop, it extracts each digit using modulo 10. If the digit is `4`, it increments a counter. Finally, it prints the count.
    Time Complexity: O(n), where `n` is the number of digits in the number.

---
+++ Assignments/Assignment 14/Program14_4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int CountFour(int iNo)
+{
+    int iDigit = 0, iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit == 4)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+//Time Complexity :O(n)
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = CountFour(iValue);
+    printf("%d", iRet);
+
+    return 0;
+}
