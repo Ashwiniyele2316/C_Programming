7f03731b79d061d3d7cf9e85c3d0da0b7df73e35
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-11-04 13:39:17 +0530

    Create Program4_5.c
    The program calculates the difference between the sum of factors and the sum of non-factors of a given number. The FactDiff function first converts negative numbers to positive. It then iterates from 1 up to one less than the input number. For each number, it checks whether it divides the input exactly. If it is a factor, it is added to iSumFact; otherwise, it is added to iSumNonFact. After completing the loop, the function returns the difference: iSumFact - iSumNonFact.
    
    The main function prompts the user to enter a number, reads it using scanf, calls FactDiff to compute the difference, and prints the result. This program demonstrates loops, conditional checks, and accumulation of values in C.

---
+++ Assignments/Assignment 4/Program4_5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int i = 0, iSumFact = 0, iSumNonFact = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i = 1; i < iNo; i++)
+    {
+        if(iNo % i == 0)
+        {
+            iSumFact = iSumFact + i;
+        }
+        else
+        {
+            iSumNonFact = iSumNonFact + i;
+        }
+    }
+
+    return iSumFact - iSumNonFact;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    iRet = FactDiff(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
