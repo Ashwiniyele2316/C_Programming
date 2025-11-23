ac4371a940b0ba9bc787ce440c0b425d9215ecce
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-11-24 00:20:54 +0530

    Create Program23_5.c

---
+++ Assignments/Assignment 23/Program23_5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Product(int Arr[], int iLength)
+{
+    int i = 0, iProd = 1;
+    int bFoundOdd = 0;
+
+    for(i = 0; i < iLength; i++)
+    {
+        if(Arr[i] % 2 != 0)     
+        {
+            iProd = iProd * Arr[i];
+            bFoundOdd = 1;
+        }
+    }
+
+    if(bFoundOdd == 0)   
+    {
+        return 0;
+    }
+
+    return iProd;
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements:\n", iSize);
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d: ", iCnt + 1);
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Product(p, iSize);
+
+    printf("Product is %d\n", iRet);
+
+    free(p);
+
+    return 0;
+}
