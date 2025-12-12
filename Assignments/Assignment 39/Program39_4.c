279d4818a71f604067120206788f4a02496096b9
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-12-12 16:23:31 +0530

    Create Program39_4.c
    This program prints the characters A to F using a recursive function called Display. The function stops when the character goes beyond 'F'. Otherwise, it prints the current character ch and then calls itself with the next character (ch + 1). The main() function begins the recursion by calling Display('A'), producing the output A B C D E F.

---
+++ Assignments/Assignment 39/Program39_4.c
@@ -0,0 +1,20 @@
+
+//Output: A  B  C  D  E  F
+
+#include<stdio.h>
+
+void Display(char ch)
+{
+    if(ch >'F')
+    {
+        return;
+    }
+    printf("%c  ",ch);
+    Display(ch+1);
+
+}
+int main()
+{
+    Display('A');
+    return 0;
+}
